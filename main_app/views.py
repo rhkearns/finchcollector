050ca2bc864d005700aa20379d417dd5449499af
rhkearns <ryankearns217@gmail.com>
2021-11-18 19:57:11 -0500

    basic urls and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+  return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
