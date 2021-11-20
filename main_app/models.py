c4f6f966aec34e5e9e014c1302fe2b6dd07daee8
rhkearns <ryankearns217@gmail.com>
2021-11-19 21:08:07 -0500

    battle with css

--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,10 @@ class Finch(models.Model):
   def get_absolute_url(self):
     return reverse("finches_detail", kwargs={"finch_id": self.id})
   
+class Sighting(models.Model):
+  date = models.DateField('Sighting Date')
+  location = models.CharField(max_length=100)
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.finch.name} spotted at {self.location} on {self.date}"
