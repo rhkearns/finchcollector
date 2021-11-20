9d189213b772e6d1c22980c6bccae281ea904d31
rhkearns <ryankearns217@gmail.com>
2021-11-19 19:31:42 -0500

    add details functionality

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,8 @@ def about(request):
 def finches_index(request):
   finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
+
+def cats_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
+
