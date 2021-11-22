5fe7c762400227453aec1e97e83dd7618856b2cd
rhkearns <ryankearns217@gmail.com>
2021-11-22 18:46:10 -0500

    add user to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,6 +22,7 @@ class Finch(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
   houses = models.ManyToManyField(House)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
   def __str__(self):
