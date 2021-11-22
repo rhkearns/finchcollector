003da22290b2228e103ca7e47090b77dccd3187b
rhkearns <ryankearns217@gmail.com>
2021-11-22 18:54:33 -0500

    add login/out functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('finches/', views.finches_index, name="finches_index"),
   path('finches/<int:finch_id>/', views.finches_detail, name='finches_detail'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
 from .models import Finch, House
 from .forms import SightingForm
 # Create your views here.
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
